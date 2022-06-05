d4d9c5b0e730b353387edab6780c7cc294bb7215
hakamdev <ehakam@student.1337.ma>
2022-06-05 02:04:32 +0100

    Implemented a couple of methods, not tested yet

--- vector/src/vector.hpp
+++ vector/src/vector.hpp
@@ -6,11 +6,12 @@
 /*   By: ehakam <ehakam@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/16 00:22:11 by ehakam            #+#    #+#             */
-/*   Updated: 2022/04/17 08:09:10 by ehakam           ###   ########.fr       */
+/*   Updated: 2022/06/04 19:46:45 by ehakam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <memory>
+#include <algorithm>
 #include <iostream>
 #include "random_access_iterator.hpp"
 #include "reverse_random_access_iterator.hpp"
@@ -35,63 +36,144 @@ namespace ft
 		typedef size_t										size_type;
 
 		private:
-			value_type		*_data;
+            pointer         _data;
+            //
 			size_type		_size;
 			size_type		_capacity;
+            //
 			allocator_type	_alloc;
-			iterator		_begin;
-			iterator		_end;
-			const_iterator	_cbegin;
-			const_iterator	_cend;
+            pointer         _begin;
+            pointer         _end;
+
 			// private helper functions
-			void _reallocate_and_copy_data( size_type _new_capacity );
-			void _init (size_type _initial_capacity, const allocator_type& alloc) {
+			void _reallocate_and_copy_data( size_type _new_capacity ) {
+				
+			}
+
+			void _init (size_type _initial_capacity) {
 				// TODO: allocate memory for *_Data using alloc
+                try {
+                    _data = _alloc.allocate(_initial_capacity);
+                } catch (std::bad_alloc& e) {
+                    std::cerr << "bad_alloc: " << e.what() << std::endl;
+                    exit(1);
+                }
 			}
 
 		public:
 			// constructors
-			explicit vector (const allocator_type& alloc = allocator_type()) : _alloc(alloc), _size(0), _capacity(0), _data(NULL) {
-				// TODO: allocate memory for *_Data using alloc
+			explicit vector (const allocator_type& alloc = allocator_type()) : _alloc(alloc), _size(0), _capacity(0) {
+                // allocate memory (0)
+                _init(_capacity);
+                
+                // reassign _begin & _end
+                _begin = NULL;
+                _end = NULL;
 			}
-			explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : _alloc(alloc) {
-				// TODO: allocate memory for *_Data using alloc
-				std::cout << "N CONS" << std::endl;
+
+			explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : _alloc(alloc), _capacity(n), _size(0) {
+                // allocate memory (n)
+                _init(_capacity);
+                
+                // construct copies of (val) in the allocated space in (_data)
+                for (size_type i = 0; i < n; ++i, ++_size) {
+                    _alloc.construct(&_data[i], val);
+                }
+                
+                // reassign _begin & _end
+                _begin = _data;
+                _end = _data[_size];
 			}
+
+            // enable_if only works if the condition inside its <> evaluates to true.
 			template <class InputIterator>
 			vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type(),
-				typename ft::enable_if<!ft::is_integral<InputIterator>::value, InputIterator>::type __t = InputIterator()) : _alloc(alloc) {
-				// typename ft::iterator_traits<InputIterator>::reference p;
-				// TODO: allocate memory for *_Data using alloc
-				std::cout << "InputIterator CONS" << std::endl;
+				typename ft::enable_if<!ft::is_integral<InputIterator>::value, InputIterator>::type __t = InputIterator()) : _alloc(alloc), _size(0) {
+                // convert the difference between (first) & (last) iterators into size_type
+                _capacity = static_cast<size_type>(last - first);
+ 
+                // allocate_memory (_capacity)
+                _init(_capacity);
+ 
+                // copy elements from first -> last
+                for (; first < last; ++_size, ++first) {
+                    _alloc.construct(&_data[_size], *first);
+                }
+                
+                // reassign _begin & _end
+                _begin = _data;
+                _end = _data[_size];
 			}
+        
 			vector (const vector& other) {
-				// TODO: copy the other vector
-				std::cout << "COPY CONS" << std::endl;
-			}
+                *this = other;
+            }
+
 			vector& operator = (const vector& other) {
-				// copy the other vector
+                this->_size = other._size;
+                this->_capacity = other._capacity;
+                this->_alloc = other._alloc;
+                
+                // allocate memory
+                _init(_capacity);
+                
+                // copy elements from other->_data
+                for (size_type i = 0; i < _size; ++i) {
+                    _alloc.construct(&_data[i], other._data[i]);
+                }
+                
+                // reassign _begin & _end
+                _begin = _data;
+                _end = _data[_size];
 			}
 			~vector() {
 				// TODO: deallocate memory using alloc
 			}
 	
 			// member functions
-			// iterator begin();
-			// const_iterator begin() const;
-			// iterator end();
-			// const_iterator end() const;
-			// reverse_iterator rbegin();
-			// const_reverse_iterator rbegin() const;
-			// reverse_iterator rend();
-			// const_reverse_iterator rend() const;
-
-			// size_type size() const;
-			// size_type max_size() const;
-			// void resize (size_type n, value_type val = value_type());
-			// size_type capacity() const;
-			// bool empty() const;
-			// void reserve (size_type n);
+            iterator begin() {
+                return iterator(_begin);
+            }
+            const_iterator begin() const {
+                return iterator(_begin);
+            }
+            iterator end() {
+                return iterator(_end);
+            }
+            const_iterator end() const {
+                return const_iterator(_end);
+            }
+            reverse_iterator rbegin() {
+                return reverse_iterator(end());
+            }
+            const_reverse_iterator rbegin() const {
+                return const_reverse_iterator(end());
+            }
+            reverse_iterator rend() {
+                return reverse_iterator(begin());
+            }
+            const_reverse_iterator rend() const {
+                return const_reverse_iterator(begin());
+            }
+            size_type size() const {
+                return static_cast<size_type>(end() - begin());
+            }
+            size_type max_size() const {
+                return std::min<size_type>(_alloc.max_size(),
+                            numeric_limits<difference_type>::max());
+            }
+            void resize (size_type n, value_type val = value_type()) {
+                // TODO: Implement
+            }
+            size_type capacity() const {
+                return _capacity;
+            }
+            bool empty() const {
+                return _size == 0;
+            }
+            void reserve (size_type n) {
+                // TODO: Implement
+            }
 
 			// reference operator[] (size_type n);
 			// const_reference operator[] (size_type n) const;
@@ -103,7 +185,7 @@ namespace ft
 			// const_reference back() const;
 
 			// template <class InputIterator>
-  			// void assign (InputIterator first, InputIterator last);	
+  			// void assign (InputIterator first, InputIterator last);
 			// void assign (size_type n, const value_type& val);
 			// void push_back (const value_type& val);
 			// void pop_back();
@@ -134,6 +216,6 @@ namespace ft
 	bool operator >= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
 
 	template <class T, class Alloc>
-  	void swap (vector<T,Alloc>& x, vector<T,Alloc>& y);
+  	void swap (vector< T, Alloc>& x, vector< T, Alloc>& y);
 
 } // namespace ft
