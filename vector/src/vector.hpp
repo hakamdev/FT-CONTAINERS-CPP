9c2dce129bafb36deeaec31b3eb77150e6723921
hakamdev <ehakam@student.1337.ma>
2022-06-09 17:11:46 +0100

    Implementing methods, now at insert

--- vector/src/vector.hpp
+++ vector/src/vector.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vector.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ehakam <ehakam@student.1337.ma>            +#+  +:+       +#+        */
+/*   By: ehakam <ehakam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/16 00:22:11 by ehakam            #+#    #+#             */
-/*   Updated: 2022/06/04 19:46:45 by ehakam           ###   ########.fr       */
+/*   Updated: 2022/06/09 17:09:40 by ehakam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,73 +36,92 @@ namespace ft
 		typedef size_t										size_type;
 
 		private:
-            pointer         _data;
+            // pointer         _begin;
             //
-			size_type		_size;
 			size_type		_capacity;
             //
 			allocator_type	_alloc;
             pointer         _begin;
             pointer         _end;
 
-			// private helper functions
-			void _reallocate_and_copy_data( size_type _new_capacity ) {
-				
-			}
-
-			void _init (size_type _initial_capacity) {
+			void _init (size_type initial_capacity) {
 				// TODO: allocate memory for *_Data using alloc
                 try {
-                    _data = _alloc.allocate(_initial_capacity);
+                    _begin = _alloc.allocate(initial_capacity);
                 } catch (std::bad_alloc& e) {
                     std::cerr << "bad_alloc: " << e.what() << std::endl;
                     exit(1);
                 }
 			}
 
+            // TODO: Move to private methods
+            void _reallocate(size_type new_capacity) {
+                size_type _current_size = size();
+                // size_type _new_capacity =
+                //     std::min<size_type>(this->_alloc.max_size(), this->_capacity * 2);
+                pointer _new_begin = this->_alloc.allocate(new_capacity);
+
+                // Copy data and delete
+                size_type i = 0;
+                for (; i < size(); ++i) {
+                    this->_alloc.construct(&_new_begin[i],this-> _begin[i]);
+                    this->_alloc.destroy(&this->_begin[i]);
+                }
+
+                // Clean old data
+                this->_alloc.deallocate(this->_begin, this->_capacity);
+
+                // reassign _begin & _begin & _end
+                this->_begin = _new_begin;
+                this->_capacity = new_capacity;
+                //this->_begin = this->_begin;
+                this->_end = this->_begin[i];
+            }
+
 		public:
 			// constructors
-			explicit vector (const allocator_type& alloc = allocator_type()) : _alloc(alloc), _size(0), _capacity(0) {
+			explicit vector (const allocator_type& alloc = allocator_type()) : _alloc(alloc), _capacity(0) {
                 // allocate memory (0)
                 _init(_capacity);
-                
+
                 // reassign _begin & _end
-                _begin = NULL;
-                _end = NULL;
+                this->_begin = NULL;
+                this->_end = NULL;
 			}
 
-			explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : _alloc(alloc), _capacity(n), _size(0) {
+			explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : _alloc(alloc), _capacity(n) {
                 // allocate memory (n)
-                _init(_capacity);
-                
-                // construct copies of (val) in the allocated space in (_data)
-                for (size_type i = 0; i < n; ++i, ++_size) {
-                    _alloc.construct(&_data[i], val);
+                _init(this->_capacity);
+
+                // construct copies of (val) in the allocated space in (_begin)
+                size_type i = 0;
+                for (; i < n; ++i) {
+                    this->_alloc.construct(&this->_begin[i], val);
                 }
-                
+
                 // reassign _begin & _end
-                _begin = _data;
-                _end = _data[_size];
+                // _begin = _begin;
+                this->_end = this->_begin[i];
 			}
 
-            // enable_if only works if the condition inside its <> evaluates to true.
 			template <class InputIterator>
 			vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type(),
-				typename ft::enable_if<!ft::is_integral<InputIterator>::value, InputIterator>::type __t = InputIterator()) : _alloc(alloc), _size(0) {
+				typename ft::enable_if<!ft::is_integral<InputIterator>::value, InputIterator>::type __t = InputIterator()) : _alloc(alloc) {
                 // convert the difference between (first) & (last) iterators into size_type
-                _capacity = static_cast<size_type>(last - first);
+                this->_capacity = static_cast<size_type>(last - first);
  
                 // allocate_memory (_capacity)
-                _init(_capacity);
+                _init(this->_capacity);
  
                 // copy elements from first -> last
-                for (; first < last; ++_size, ++first) {
-                    _alloc.construct(&_data[_size], *first);
+                size_type i = 0;
+                for (; first < last; ++i, ++first) {
+                    this->_alloc.construct(&this->_begin[i], *first);
                 }
-                
+
                 // reassign _begin & _end
-                _begin = _data;
-                _end = _data[_size];
+                // _begin = _begin;
+                this->_end = this->_begin[i];
 			}
         
 			vector (const vector& other) {
@@ -110,96 +129,186 @@ namespace ft
             }
 
 			vector& operator = (const vector& other) {
-                this->_size = other._size;
                 this->_capacity = other._capacity;
                 this->_alloc = other._alloc;
                 
                 // allocate memory
-                _init(_capacity);
+                _init(this->_capacity);
                 
-                // copy elements from other->_data
-                for (size_type i = 0; i < _size; ++i) {
-                    _alloc.construct(&_data[i], other._data[i]);
+                // copy elements from other->_begin
+                size_type i = 0;
+                for (; i < size(); ++i) {
+                    this->_alloc.construct(&this->_begin[i], other._begin[i]);
                 }
-                
+
                 // reassign _begin & _end
-                _begin = _data;
-                _end = _data[_size];
+                // _begin = _begin;
+                this->_end = this->_begin[i];
 			}
+
 			~vector() {
 				// TODO: deallocate memory using alloc
 			}
 	
 			// member functions
             iterator begin() {
-                return iterator(_begin);
+                return iterator(this->_begin);
             }
+
             const_iterator begin() const {
-                return iterator(_begin);
+                return iterator(this->_begin);
             }
+            
             iterator end() {
-                return iterator(_end);
+                return iterator(this->_end);
             }
+            
             const_iterator end() const {
-                return const_iterator(_end);
+                return const_iterator(this->_end);
             }
+            
             reverse_iterator rbegin() {
                 return reverse_iterator(end());
             }
+            
             const_reverse_iterator rbegin() const {
                 return const_reverse_iterator(end());
             }
+            
             reverse_iterator rend() {
                 return reverse_iterator(begin());
             }
+            
             const_reverse_iterator rend() const {
                 return const_reverse_iterator(begin());
             }
+            
             size_type size() const {
                 return static_cast<size_type>(end() - begin());
             }
+            
             size_type max_size() const {
-                return std::min<size_type>(_alloc.max_size(),
+                return std::min<size_type>(this->_alloc.max_size(),
                             numeric_limits<difference_type>::max());
             }
-            void resize (size_type n, value_type val = value_type()) {
+            
+            void resize(size_type n, value_type val = value_type()) {
                 // TODO: Implement
             }
+            
             size_type capacity() const {
-                return _capacity;
+                return this->_capacity;
             }
+
             bool empty() const {
-                return _size == 0;
+                return size() == 0;
             }
-            void reserve (size_type n) {
+
+            void reserve(size_type n) {
                 // TODO: Implement
             }
 
-			// reference operator[] (size_type n);
-			// const_reference operator[] (size_type n) const;
-			// reference at (size_type n);
-			// const_reference at (size_type n) const;
-			// reference front();
-			// const_reference front() const;
-			// reference back();
-			// const_reference back() const;
+			reference operator[] (size_type n) {
+                return this->_begin[n];
+            }
 
-			// template <class InputIterator>
-  			// void assign (InputIterator first, InputIterator last);
-			// void assign (size_type n, const value_type& val);
-			// void push_back (const value_type& val);
-			// void pop_back();
+			const_reference operator[] (size_type n) const {
+                return this->_begin[n];
+            }
 
-			// template <class InputIterator>
-    		// void insert (iterator position, InputIterator first, InputIterator last);
-			// iterator insert (iterator position, const value_type& val);
-    		// void insert (iterator position, size_type n, const value_type& val);	
+			reference at (size_type n) {
+                return this->_begin[n];
+            }
+
+			const_reference at (size_type n) const {
+                return this->_begin[n];
+            }
+
+			reference front() {
+                return this->_begin[0];
+            }
+
+			const_reference front() const {
+                return this->_begin[0];
+            }
+
+			reference back() {
+                return *(this->_end - 1);
+            }
+
+			const_reference back() const {
+                return *(this->_end - 1);
+            }
+
+			template <class InputIterator>
+  			void assign(InputIterator first, InputIterator last) {
+                // TODO: implement
+            }
+
+			void assign(size_type n, const value_type& val) {
+                // TODO: implement
+            }
+
+			void push_back(const value_type& val) {
+                if (size() >= this->_capacity) {
+                    size_type _new_capacity =
+                        std::min<size_type>(this->_alloc.max_size(), this->_capacity * 2);
+                    _reallocate(_new_capacity);
+                }
+                this->_alloc.construct(this->_end, val);
+                ++this->_end;
+            }
+
+			void pop_back() {
+                if (empty()) return;
+                this->_alloc.destroy(this->_end - 1);
+                --this->_end;
+            }
+
+			template <class InputIterator>
+    		void insert (iterator position, InputIterator first, InputIterator last) {
+                // TODO: implement
+                pointer     _p = this->_begin + (position - begin());
+                size_type   _diff = static_cast<size_type>(last - first);
+                if (size() + _diff > this->_capacity) {
+                    _reallocate();
+                }
+
+                for(size_type i = size() - 1; )
+            }
+
+			iterator insert (iterator position, const value_type& val) {
+                // TODO: implement
+                pointer _p = this->_begin + (position - begin());
+                
+                
+                
+            }
+
+    		void insert (iterator position, size_type n, const value_type& val) {
+                // TODO: implement
+                pointer _p = this->_begin + (position - begin());
+            }
+
+			iterator erase (iterator position) {
+                // TODO: implement
+            }
+
+			iterator erase (iterator first, iterator last) {
+                // TODO: implement
+            }
+
+			void swap (vector& x) {
+                // TODO: implement
+            }
 			
-			// iterator erase (iterator position);
-			// iterator erase (iterator first, iterator last);
-			// void swap (vector& x);
-			// void clear();
-			// allocator_type get_allocator() const;
+            void clear() {
+                // TODO: implement
+            }
+
+			allocator_type get_allocator() const {
+                return _alloc;
+            }
 	};
 
 	template <class T, class Alloc>
