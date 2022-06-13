39f14eaa7e316d83ef4e7c7e494164e086b686e5
hakamdev <ehakam@student.1337.ma>
2022-06-13 02:20:48 +0100

    map: add bidirectional and reverse bidirectional iterators.

--- map/src/reverse_bidirectional_iterator.hpp
+++ map/src/reverse_bidirectional_iterator.hpp
@@ -1,27 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   bidirectional_iterator.hpp                         :+:      :+:    :+:   */
+/*   reverse_bidirectional_iterator.hpp                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ehakam <ehakam@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/12 22:54:07 by ehakam            #+#    #+#             */
-/*   Updated: 2022/06/13 02:13:10 by ehakam           ###   ########.fr       */
+/*   Created: 2022/06/13 01:42:02 by ehakam            #+#    #+#             */
+/*   Updated: 2022/06/13 02:19:45 by ehakam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __BIDIR_ITER_HPP__
-# define __BIDIR_ITER_HPP__
+#ifndef __RBIDIR_ITER_HPP__
+# define __RBIDIR_ITER_HPP__
 
 # include <iterator>
 # include "iterator.hpp"
 # include "traits.hpp"
+# include "bidirectional_iterator.hpp"
 
 namespace ft
 {
 	template <typename Iter>
-	struct bidir_iterator : public ft::iterator<std::bidirectional_iterator_tag, Iter> {
-
+	struct rbidir_iterator : public ft::iterator<std::bidirectional_iterator_tag, Iter> {
+		
 		typedef Iter															iterator_type;
 		typedef typename ft::iterator_traits<iterator_type>::value_type			value_type;
 		typedef typename ft::iterator_traits<iterator_type>::difference_type	difference_type;
@@ -32,42 +33,44 @@ namespace ft
 			iterator_type _base;
 
 		// Constructors / Destructor
-		bidir_iterator( void ) : _base() {}
-		bidir_iterator( iterator_type base ) : _base(base) {}
-		bidir_iterator( bidir_iterator<iterator_type> const & copy ) { *this = copy; }
+		rbidir_iterator( void ) : _base() {}
+		rbidir_iterator( iterator_type base ) : _base(base) {}
+		rbidir_iterator( rbidir_iterator<iterator_type> const & copy ) { *this = copy; }
 		template <typename T2>
-		bidir_iterator( bidir_iterator<T2> const & copy ) { this->_base = copy.base(); }
-		bidir_iterator& operator = ( bidir_iterator<Iter> const & copy ) { this->_base = copy.base(); return (*this); }
-		~bidir_iterator() {}
+		rbidir_iterator( rbidir_iterator<T2> const & copy ) { this->_base = copy.base(); }
+		rbidir_iterator& operator = ( rbidir_iterator<Iter> const & copy ) { this->_base = copy.base(); return (*this); }
+		~rbidir_iterator() {}
 
 		iterator_type base() const { return _base; }
 
 		// Overloaded operators
-		bool operator == ( racc_iterator const & other ) {
+		bool operator == ( rbidir_iterator const & other ) {
 			return (this->_base == other._base);
 		}
-		bool operator != ( racc_iterator const & other ) {
+		bool operator != ( rbidir_iterator const & other ) {
 			return (this->_base != other._base);
 		}
 		reference operator * () {
-			return (*this->_base);
-		}
-		bidir_iterator& operator ++ () {
+			iterator_type _correct = --this->_base;
 			++this->_base;
+			return (*_correct);
+		}
+		rbidir_iterator& operator ++ () {
+			--this->_base;
 			return (*this);
 		}
-		bidir_iterator operator ++ ( int ) {
-			bidir_iterator<Iter> old(*this);
-			++this->_base;
+		rbidir_iterator operator ++ ( int ) {
+			rbidir_iterator<iterator_type> old(*this);
+			--this->_base;
 			return (old);
 		}
-		bidir_iterator& operator -- () {
-			--this->_base;
+		rbidir_iterator& operator -- () {
+			++this->_base;
 			return (*this);
 		}
-		bidir_iterator operator -- ( int ) {
-			bidir_iterator<Iter> old(*this);
-			--this->_base;
+		rbidir_iterator operator -- ( int ) {
+			rbidir_iterator<iterator_type> old(*this);
+			++this->_base;
 			return (old);
 		}
 	};
